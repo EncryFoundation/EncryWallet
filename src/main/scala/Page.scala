7253d5f73d8f7a779490b005ef0e1f439d404e1a
Yury Ignatev <yury.ignatev@gmail.com>
2018-06-18 16:16:59 +0300

    some frontend

--- src/main/scala/Page.scala
+++ src/main/scala/Page.scala
@@ -2,8 +2,20 @@ import scalatags.Text
 import scalatags.Text.all._
 
 object Page {
-  val integrity: Attr = attr("integrity")
-  val crossorigin = attr("crossorigin")
+  def accountForm = form()(
+    div( cls:="form-group")(
+      label(`for`:="publicKey")("Amount"),
+      input(tpe:="number", cls:="form-control", id:="publicKey", placeholder:="0", name:="amount")
+    ),
+    div( cls:="form-group")(
+      label(`for`:="exampleAddressInput")("Address"),
+      input(tpe:="text", cls:="form-control", id:="exampleAddressInput", placeholder:="Enter address here",name:="address"),
+    ),
+    button(tpe:="submit", cls:="btn btn-primary")("Submit")
+  )
+
+  lazy val integrity: Attr = attr("integrity")
+  lazy val crossorigin = attr("crossorigin")
   def page(el: Modifier*) = html(
     head(
       script(src:="https://code.jquery.com/jquery-3.2.1.slim.min.js",
@@ -22,15 +34,63 @@ object Page {
     body(el)
   )
 
-  val modalButton =
-    div(cls:="jumbotron")(
-      button(tpe:="button", cls:="btn btn-primary", attr("data-toggle"):="modal",
-        attr("data-target"):="#myModal")(
-        "Send money"
-      )
+  lazy val transactionFormInner = Seq(
+    div( cls:="form-group")(
+      label(`for`:="exampleCurrencyInput")("Private key:"),
+      input(tpe:="text", cls:="form-control", id:="exampleCurrencyInput", placeholder:="ABCDEF", name:="prKey", disabled)
+    ),
+    div( cls:="form-group")(
+      label(`for`:="exampleCurrencyInput")("Fee"),
+      input(tpe:="number", cls:="form-control", id:="exampleCurrencyInput", placeholder:="0", name:="fee")
+    ),
+    div( cls:="form-group")(
+      label(`for`:="exampleCurrencyInput")("Change"),
+      input(tpe:="number", cls:="form-control", id:="exampleCurrencyInput", placeholder:="0", name:="change")
+    ),
+    div( cls:="form-group")(
+      label(`for`:="exampleCurrencyInput")("Amount"),
+      input(tpe:="number", cls:="form-control", id:="exampleCurrencyInput", placeholder:="0", name:="amount")
+    ),
+    div( cls:="form-group")(
+      label(`for`:="exampleAddressInput")("Address"),
+      input(tpe:="text", cls:="form-control", id:="exampleAddressInput", placeholder:="Enter address here",name:="recepient"),
+    ),
   )
 
-  val modalForm = form()(
+  val contractInput = div( cls:="form-group")(
+    label(`for`:="exampleContractInput")("Contract"),
+    textarea(tpe:="text", cls:="form-control", id:="exampleContractInput", placeholder:="ScriptCode",name:="recepient"),
+  )
+
+  lazy val submitButton = button(tpe:="submit", cls:="btn btn-primary")("Submit")
+
+  lazy val transactionForm1 = form(transactionFormInner)(submitButton)
+  lazy val transactionForm2 = form(transactionFormInner)(textarea, submitButton)
+
+  lazy val buttonGroup =
+    div(cls:="btn-group-vertical")(
+      modalButton("myModal")("Transaction1"),
+      modalButton("myModal2")("Transaction2"),
+      modalButton("myModal2")("Transaction3")
+    )
+
+  lazy val layout = div(cls:="container-fluid")(
+    div(cls:="row")(
+      div(cls:="col-3")(buttonGroup),
+      div(cls:="col-9"),
+    )
+  )
+
+  def modalButton(modalId: String) = button(tpe:="button", cls:="btn btn-primary", attr("data-toggle"):="modal",
+    attr("data-target"):=s"#$modalId")
+  lazy val jumbModalButton =
+    div(cls:="jumbotron")(modalButton("SendMoney"))
+
+  lazy val modalForm = form()(
+    div( cls:="form-group")(
+      label(`for`:="exampleCurrencyInput")("Private key:"),
+      input(tpe:="text", cls:="form-control", id:="exampleCurrencyInput", placeholder:="ABCDEF", name:="amount", disabled)
+    ),
     div( cls:="form-group")(
       label(`for`:="exampleCurrencyInput")("Amount"),
       input(tpe:="number", cls:="form-control", id:="exampleCurrencyInput", placeholder:="0", name:="amount")
@@ -42,7 +102,9 @@ object Page {
     button(tpe:="submit", cls:="btn btn-primary")("Submit")
   )
 
-  val modal: Modifier =
+  lazy val example = page(layout,modal,modal2)
+
+  lazy val modal: Modifier =
     div(cls:="modal", id:="myModal")(
       div(cls:="modal-dialog")(
         div(cls:="modal-content")(
@@ -50,11 +112,28 @@ object Page {
             h4(cls:="modal-title")("Transfer"),
             button(tpe:="button", cls:="close", attr("data-dissmiss"):="modal")(raw("&times;"))
           ),
-          div(cls:="modal-body")(modalForm),
-          div(cls:="modal-footer")(
-            button(tpe:="button",cls:="btn btn-primary")("Save changes"),
-            button(tpe:="button",cls:="btn btn-secondary", attr("data-dissmiss"):="modal")("Close"),
-          )
+          div(cls:="modal-body")(transactionForm1),
+//          div(cls:="modal-footer")(
+//            button(tpe:="button",cls:="btn btn-primary")("Save changes"),
+//            button(tpe:="button",cls:="btn btn-secondary", attr("data-dissmiss"):="modal")("Close"),
+//          )
+        )
+      )
+    )
+
+  lazy val modal2: Modifier =
+    div(cls:="modal", id:="myModal2")(
+      div(cls:="modal-dialog")(
+        div(cls:="modal-content")(
+          div(cls:="modal-header")(
+            h4(cls:="modal-title")("Transfer"),
+            button(tpe:="button", cls:="close", attr("data-dissmiss"):="modal")(raw("&times;"))
+          ),
+          div(cls:="modal-body")(transactionForm2),
+          //          div(cls:="modal-footer")(
+          //            button(tpe:="button",cls:="btn btn-primary")("Save changes"),
+          //            button(tpe:="button",cls:="btn btn-secondary", attr("data-dissmiss"):="modal")("Close"),
+          //          )
         )
       )
     )
--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -1,8 +1,14 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpHeader.ParsingResult.Ok
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import encry.account.Address
+import encry.crypto.{PrivateKey25519, PublicKey25519}
+import encry.modifiers.mempool.{EncryTransaction, TransactionFactory}
+//import encry.
+
 import scala.io.StdIn
 
 object WebServer {
@@ -14,23 +20,37 @@ object WebServer {
     implicit val executionContext = system.dispatcher
 
     import Page._
+    import ExtUtils._
     val data = Map.empty[String,Number]
     val route =
       path(""){
         get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, page(modalButton, modal).render))
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, page(jumbModalButton, modal).render))
         }
+//      } ~ path("send") {
+//        get {
+//          parameter('amount.as[Int], 'address.as[String] ){ (amount, address) =>
+//            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, page(modalButton, modal).render))
+//          }
+//        }
       } ~ path("send") {
-        get {
-          parameter('amount.as[Int], 'address.as[String] ){ (amount, address) =>
-            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, page(modalButton, modal).render))
-          }
+        parameters('recepient, 'fee.as[Long], 'change.as[Long], 'amount.as[Long]) { (recepient, fee, change, amount) =>
+          val (prKey, pubKey) = PrivateKey25519.generateKeys("1".getBytes)
+          val transaction: EncryTransaction = TransactionFactory.defaultPaymentTransactionScratch(
+            prKey, fee, System.currentTimeMillis, ???, recepient.asInstanceOf[Address], amount, None)
+          import encry.modifiers.mempool.EncryTransaction._
+          import io.circe.syntax._
+          transaction.asJson.trace
+
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, page(jumbModalButton, modal).render))
         }
+      } ~ path("test" ) {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, example.render))
       }
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+      println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
