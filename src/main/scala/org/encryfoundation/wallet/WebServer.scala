2fd5ea4bb8f4afb6e031d677db225a5307c3f6e7
Yury Ignatev <yury.ignatev@gmail.com>
2018-06-20 16:51:54 +0300

    refactor

--- src/main/scala/org/encryfoundation/wallet/WebServer.scala
+++ src/main/scala/org/encryfoundation/wallet/WebServer.scala
@@ -4,16 +4,18 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpEntity, _}
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.StandardRoute
 import akka.stream.ActorMaterializer
 import akka.util.ByteString
 import io.circe.parser._
 import org.encryfoundation.wallet.crypto.PrivateKey25519
-import org.encryfoundation.wallet.transaction.Transaction
+import org.encryfoundation.wallet.transaction.{EncryTransaction, Transaction}
 import org.encryfoundation.wallet.transaction.box.AssetBox
 import org.encryfoundation.wallet.transaction.box.AssetBox._
 import scorex.crypto.encode.Base58
 import scorex.crypto.signatures.PrivateKey
 
+import scala.collection.immutable
 import scala.concurrent._
 import scala.io.StdIn
 import scala.util.Success
@@ -36,46 +38,54 @@ object WebServer {
 
     val nodeHost = "http://172.16.10.58:9051"
 
+    def getBoxesFromNode(address: String, amountAndFee: Long): Future[immutable.IndexedSeq[AssetBox]] =
+      Uri(s"$nodeHost/account/$address/boxes")
+        .rapply(uri => HttpRequest(uri = uri))
+        .rapply(Http().singleRequest(_))
+        .flatMap(_.entity.dataBytes.runFold(ByteString.empty)(_ ++ _))
+        .map(_.utf8String)
+        .map(decode[Seq[AssetBox]])
+        .map(_.map(_.foldLeft(Seq[AssetBox]()) {
+          case (seq, box) =>
+            if (seq.map(_.value).sum < amountAndFee) seq :+ box else seq
+        }.toIndexedSeq))
+      .flatMap{
+        case Right(x) => Future.successful(x)
+        case Left(e) => Future.failed(e)
+      }
+
+    def sendTransactionsToNode(encryTransaction: EncryTransaction) =
+      encryTransaction.asJson.toString
+      .rapply(HttpEntity(ContentTypes.`text/html(UTF-8)`,_))
+      .rapply(HttpRequest(method = HttpMethods.POST, uri = Uri(s"$nodeHost/transactions/send")).withEntity(_))
+      .rapply(Http().singleRequest(_))
+      .map(_.status)
+
+    def pageRoute: StandardRoute = complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, walletData.view.render))
+
+    def sendTransaction (fee: Long, amount: Long, recepient: String): Future[HttpResponse] =
+      walletData.wallet.map{ wallet =>
+      getBoxesFromNode(recepient, fee + amount).flatMap{ boxes =>
+        Transaction.defaultPaymentTransactionScratch(wallet.getSecret, fee, System.currentTimeMillis, boxes,
+          recepient, amount, None).asJson.toString
+          .rapply(HttpEntity(ContentTypes.`text/html(UTF-8)`,_))
+          .rapply(HttpRequest(method = HttpMethods.POST, uri = Uri(s"$nodeHost/transactions/send")).withEntity(_))
+          .rapply(Http().singleRequest(_))
+      }
+    }.getOrElse(Future.failed(new Exception("Send transaction without wallet")))
+
+
     val route =
       path("") {
-        parameters('fee.as[Long], 'amount.as[Long],'recepient.as[String]) { (fee, amount, recepient) =>
-          val useboxes = walletData.wallet.map(_.account.address).map(
-            address => Uri(s"$nodeHost/account/$address/boxes")
-          ).map( uri => Http().singleRequest(
-            HttpRequest(uri = uri)
-          ) .flatMap(_.entity.dataBytes.runFold(ByteString.empty)(_ ++ _))
-            .map(_.utf8String)
-            .map(decode[Seq[AssetBox]])
-            //.map(_ => Right(Seq.empty[AssetBox]))
-            .map(_.map(_.foldLeft(Seq[AssetBox]()) { case (seq, box) =>
-              if (seq.map(_.value).sum < (amount + fee)) seq :+ box else seq
-            }.toIndexedSeq.trace
-            )
-            )
-          ).getOrElse(Future.failed(new Exception("Empty wallet")))
-          onComplete(useboxes){
-            case Success(Right(boxes))  =>
-              val sendData = walletData.wallet.map(wallet =>
-                Transaction.defaultPaymentTransactionScratch(
-                  wallet.getSecret, fee, System.currentTimeMillis, boxes, recepient, amount, None)
-                    .asJson.trace)
-                .map(  json =>
-                  HttpRequest(HttpMethods.POST, Uri(s"$nodeHost/transactions/send"),
-                    entity = HttpEntity(ContentTypes.`text/html(UTF-8)`, json.toString))
-//                    entity = HttpEntity(ContentTypes.`text/html(UTF-8)`, walletData.view.render)
-                ).map(x => Http().singleRequest(request = x)).get
-              onComplete(sendData) { x =>
-                complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, walletData.view.render))
-              }
-            case x => complete(StatusCodes.ExpectationFailed).trace(x)
-          }
+        parameters('fee.as[Long], 'amount.as[Long], 'recepient.as[String]) { (fee,amount,recepient) =>
+          onSuccess( sendTransaction(fee,amount,recepient))(_ => pageRoute)
         }
       } ~ {
         path("") {
           parameters('privateKey.as[String].?) { privateKey =>
             val wallet: Option[Wallet] = privateKey.flatMap(Base58.decode(_).toOption).map(x => Wallet.initWithKey(PrivateKey @@ x))
             if (wallet.isDefined) walletData = walletData.copy(wallet = wallet).trace
-            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, walletData.view.render))
+            pageRoute
           }
         }
       }//~ complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, walletData.view.render))
