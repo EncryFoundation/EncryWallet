f3156d19579ca4e658ac2b7dc4c6af34b066240d
Yury Ignatev <yury.ignatev@gmail.com>
2018-06-21 10:39:00 +0300

    script transaction

--- src/main/scala/org/encryfoundation/wallet/WebServer.scala
+++ src/main/scala/org/encryfoundation/wallet/WebServer.scala
@@ -12,29 +12,39 @@ import org.encryfoundation.wallet.crypto.PrivateKey25519
 import org.encryfoundation.wallet.transaction.box.AssetBox
 import org.encryfoundation.wallet.transaction.box.AssetBox._
 import org.encryfoundation.wallet.transaction.{EncryTransaction, Transaction}
+import scorex.crypto.authds.ADKey
 import scorex.crypto.encode.Base58
 import scorex.crypto.signatures.PrivateKey
 
 import scala.collection.immutable
 import scala.concurrent._
 import scala.io.StdIn
+import io.circe.syntax._
+
+
+case class TransactionWrapper(privKey: PrivateKey25519,
+                         fee: Long,
+                         timestamp: Long,
+                         recipient: String,
+                         amount: Long,
+                         tokenIdOpt: Option[ADKey] = None){
+  def default: IndexedSeq[AssetBox] => EncryTransaction = useBoxes =>
+    Transaction.defaultPaymentTransactionScratch( privKey, fee, timestamp, useBoxes, recipient, amount, tokenIdOpt)
+  def withContract(src: String): IndexedSeq[AssetBox] => EncryTransaction = useBoxes =>
+    Transaction.scriptedAssetTransactionScratch(privKey, fee, timestamp, useBoxes, ???, amount, tokenIdOpt)
+}
 
 object WebServer {
   def main(args: Array[String]): Unit = {
 
     implicit val system: ActorSystem = ActorSystem("my-system")
     implicit val materializer: ActorMaterializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
     import org.encryfoundation.wallet.utils.ExtUtils._
 
     val (_, pub) = PrivateKey25519.generateKeys("1".getBytes)
     var walletData = WalletData( None, pub, "3BxEZq6XcBzMSoDbfmY1V9qoYCwu73G1JnJAToaYEhikQ3bBKK")
-
-    import io.circe.syntax._
-
-
     val nodeHost = "http://172.16.10.58:9051"
 
     def getBoxesFromNode(address: String, amountAndFee: Long): Future[immutable.IndexedSeq[AssetBox]] =
@@ -58,29 +68,38 @@ object WebServer {
         .rapply(HttpEntity(ContentTypes.`text/html(UTF-8)`,_))
         .rapply(HttpRequest(method = HttpMethods.POST, uri = Uri(s"$nodeHost/transactions/send")).withEntity(_))
         .rapply(Http().singleRequest(_))
-        .map(_.status)
 
     def pageRoute: StandardRoute = complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, walletData.view.render))
 
     def sendTransaction (fee: Long, amount: Long, recepient: String): Future[HttpResponse] =
       walletData.wallet.map{ wallet =>
         getBoxesFromNode(recepient, fee + amount).flatMap{ boxes =>
-          Transaction.defaultPaymentTransactionScratch(wallet.getSecret, fee, System.currentTimeMillis, boxes,
-            recepient, amount, None).asJson.toString
-            .rapply(HttpEntity(ContentTypes.`text/html(UTF-8)`,_))
-            .rapply(HttpRequest(method = HttpMethods.POST, uri = Uri(s"$nodeHost/transactions/send")).withEntity(_))
-            .rapply(Http().singleRequest(_))
+          //new TransactionWrapper(wallet.getSecret, fee, System.currentTimeMillis, recepient, amount))
+          Transaction.defaultPaymentTransactionScratch(wallet.getSecret, fee, System.currentTimeMillis, boxes, recepient, amount, None)
+            .rapply(sendTransactionsToNode)
         }
       }.getOrElse(Future.failed(new Exception("Send transaction without wallet")))
 
+    def sendTransactionScript (fee: Long, amount: Long, recepient: String, src: String): Future[HttpResponse] =
+    walletData.wallet.map{ wallet =>
+      getBoxesFromNode(recepient, fee + amount).flatMap{ boxes =>
+        val compiled = ???
+        Transaction.scriptedAssetTransactionScratch(wallet.getSecret, fee, System.currentTimeMillis, boxes, compiled, amount, None)
+          .rapply(sendTransactionsToNode)
+      }
+    }.getOrElse(Future.failed(new Exception("Send transaction without wallet")))
 
     val route =
-      path("") {
+      path("send"/"address") {
         parameters('fee.as[Long], 'amount.as[Long], 'recepient.as[String]) { (fee,amount,recepient) =>
           onSuccess( sendTransaction(fee,amount,recepient))(_ => pageRoute)
         }
+      } ~ path("send"/"contract") {
+        parameters('fee.as[Long], 'amount.as[Long], 'recepient.as[String], 'src.as[String]) { (fee,amount,recepient, src) =>
+          onSuccess( sendTransactionScript(fee,amount,recepient,src))(_ => pageRoute)
+        }
       } ~ {
-        path("") {
+        path("settings") {
           parameters('privateKey.as[String].?) { privateKey =>
             val wallet: Option[Wallet] = privateKey.flatMap(Base58.decode(_).toOption).map(x => Wallet.initWithKey(PrivateKey @@ x))
             if (wallet.isDefined) walletData = walletData.copy(wallet = wallet).trace
