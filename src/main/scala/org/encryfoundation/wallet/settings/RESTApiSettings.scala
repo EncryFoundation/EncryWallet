b2d4be02bab18ce8ee2b4eb9024b5b9fa8ab5839
Stanislav Kapinus <kapinuss@rambler.ru>
2018-07-24 11:45:45 +0300

    ec-wallet-refactoring (#12)
    * deletion
    
    * sbt
    
    * -public

--- src/main/scala/org/encryfoundation/wallet/settings/RESTApiSettings.scala
+++
@@ -1,8 +0,0 @@
-package org.encryfoundation.wallet.settings
-
-import java.net.InetSocketAddress
-
-import scala.concurrent.duration.FiniteDuration
-
-case class RESTApiSettings(bindAddress: InetSocketAddress,
-                           timeout: FiniteDuration)
